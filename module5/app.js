61333005a33af100685773a612306b7e74404f20
Victor Fedorov <vpfedorov25@gmail.com>
2022-02-05 20:52:03 +0300

    section 5 was finished

---
+++ module5/app.js
@@ -0,0 +1,13 @@
+// let name = "john";
+//
+// function boo() {
+//     let name = "john";
+//     console.log(name);
+// }
+//
+// boo();
+// console.log(name);
+
+console.log(name);
+
+var name = "Victor";
